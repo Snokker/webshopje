e53b34f3441f99d5460d0f81a50c9b9a3d553144
Lisa <lisagameart@gmail.com>
2018-07-26 16:57:20 +0200

    halverwege tutorial 2,  van https://www.youtube.com/watch?v=riSKBb7KbFI shopingcart express-handlebars geinstalleerd, het werkt nu. ik maa een sikke save

--- shopshop/app.js
+++ shopshop/app.js
@@ -1,17 +1,32 @@
+// toturial: https://www.youtube.com/watch?v=-3vvxn78MH4
+
+//https://nodejs.org/en/
+//https://expressjs.com/en/starter/generator.html
+//https://getbootstrap.com/docs/4.1/getting-started/download/
+//https://code.jquery.com/
+
+// opzet: zet snel een node/express app op.
+// npm install express-generator -g (global)
+// ga in cmd naar de map waar je in wilt zijn en doe...express "naam nieuwe map" --hbs (handelbars template)
+// cd in de "naam nieuwe map" npm install
+// npm start (localhost:3000)
+// nieuwe terminal en doe in folder npm install --save express-handebars
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//onthoud dat in het begin iets anders is gegaan, de jason-lock
+var expressHbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +35,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
